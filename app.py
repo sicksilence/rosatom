6bfdb2411d55f22e7f754093ea799146c6307885
sicksilence <vitaincerta187@gmail.com>
2024-03-20 12:53:15 +0300

    а

--- app.py
+++ app.py
@@ -8,7 +8,15 @@ cartridges = ["Картридж 1", "Картридж 2", "Картридж 3"]
 
 @app.route('/')
 def index():
-    return render_template('index.html',cartridge=cartridges)
+    # Прочитать данные из файла Excel
+    try:
+        df = pd.read_excel("список_картриджей.xlsx")
+        # Преобразовать данные в формат, который можно передать в HTML-шаблон
+        data = df.to_dict(orient='records')
+    except Exception as e:
+        print("Ошибка чтения файла Excel:", e)
+        data = None
+    return render_template('index.html', data=data)
 
 @app.route('/submit', methods=['POST'])
 def submit():
